c58a248ab09b0f1c03351304578851c295e39f80
Caio Lopes <caiolopeis95@gmail.com>
2024-11-25 10:55:25 -0300

    style: criação do header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import {Image,View,} from "react-native" 
+import {Image,TouchableOpacity,View,} from "react-native" 
 import styles from "./styles";
-
+import{MaterialIcons} from '@expo/vector-icons'
+import {colors} from "@/styles/colors"
 
 export default function Index (){
     return (
         <View style={styles.container}>
-       <Image source={require('@/assets/logo.png')}/>
-        
+            <View style={styles.header}>
+       <Image source={require('@/assets/logo.png')} style={styles.logo}/>
+       <TouchableOpacity>
+        <MaterialIcons name="add" size={32} color={colors.green[300]}/>
+        </TouchableOpacity>
+        </View>
         </View>
     )
 }
