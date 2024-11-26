d1871db98d72fb1c7592ffc4d8c6ac4530940d8b
Caio Lopes <caiolopeis95@gmail.com>
2024-11-26 18:37:53 -0300

    feat:criando modal

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Image,TouchableOpacity,View,FlatList} from "react-native" 
+import {Text,TouchableOpacity,View,FlatList,Modal} from "react-native" 
 import styles from "./styles";
 import{MaterialIcons} from '@expo/vector-icons'
 import {colors} from "@/styles/colors"
@@ -26,6 +26,21 @@ export default function Index (){
          contentContainerStyle={styles.linkContent}
          showsVerticalScrollIndicator={false}
          />
+         <Modal transparent visible={true}>
+            <View style={styles.modal}>
+                <View style={styles.modalContent}>
+                    <View style={styles.modalHeader}>
+                        <Text style={styles.modalCategory}>Curso</Text>
+                        <TouchableOpacity>
+                        <MaterialIcons name="close" size={20} color={colors.gray[400]}/>
+                        </TouchableOpacity>
+                        
+                    </View>
+                    <Text style={styles.modalLinkName}></Text>
+                    <Text style={styles.modalUrl}>https://rocketseat.com.br/</Text>
+                </View>
+            </View>
+         </Modal>
         </View>
     )
 }
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -30,7 +30,39 @@ marginBottom:32
     gap:20,
     padding:24,
     paddingBottom:100
-  }
+  },
+  modal:{flex:1,
+    justifyContent:'flex-end'
+
+  },
+  modalContent:{
+    backgroundColor:colors.gray[900],
+    borderTopColor:colors.gray[800],
+    borderTopWidth:1,
+    paddingBottom:42,
+    padding:24
+  },
+  modalHeader:{
+    width:'100%',
+    flexDirection:'row',
+    alignItems:'center',
+    marginBottom:32
+  },
+  modalCategory:{
+    flex:1,
+    fontSize:16,
+    fontWeight:'500',
+    color:colors.gray[400]
+  },
+  modalLinkName:{
+    fontSize:18,
+    fontWeight:'600',
+    color:colors.gray[200]
+  },
+  modalUrl:{
+    fontSize:14,
+    color:colors.gray[400]
+  },
 
 });
 
