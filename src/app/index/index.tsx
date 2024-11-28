8dc01efbb80b0faef79472a0378ea46ecf38edc0
Caio Lopes <caiolopeis95@gmail.com>
2024-11-28 10:41:37 -0300

    refactor:ajeitando header da index e craindo add

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Text,TouchableOpacity,View,FlatList,Modal} from "react-native" 
+import {Text,TouchableOpacity,View,FlatList,Modal,Image} from "react-native" 
 import styles from "./styles";
 import{MaterialIcons} from '@expo/vector-icons'
 import {colors} from "@/styles/colors"
@@ -10,7 +10,12 @@ import { Option } from "@/components/option";
 export default function Index (){
     return (
         <View style={styles.container}>
+           
             <View style={styles.header}>
+            <Image source={require('@/assets/logo.png')} style={styles.logo}/>
+       <TouchableOpacity>
+        <MaterialIcons name="add" size={32} color={colors.green[300]}/>
+        </TouchableOpacity>
             </View>
         <Categories/>
         
@@ -37,7 +42,7 @@ export default function Index (){
                         </TouchableOpacity>
                         
                     </View>
-                    <Text style={styles.modalLinkName}></Text>
+                    <Text style={styles.modalLinkName}>RocketSeat</Text>
                     <Text style={styles.modalUrl}>https://rocketseat.com.br/</Text>
                     <View style={styles.modalFooter}>
                        <Option name='Excluir' icon='delete' variant='secondary'/>
