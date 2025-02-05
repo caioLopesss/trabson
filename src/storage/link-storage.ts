f5a95d126c2c01ba7a05756c69d69f16699ac4bd
Caio Lopes <caiolopeis95@gmail.com>
2025-02-05 14:53:00 -0300

    feat: armazenando os links

---
+++ src/storage/link-storage.ts
@@ -0,0 +1,26 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const LINKS_STORAGE_KEY = "links-storage";
+
+type LinkStorage = {
+  id: string;
+  name: string;
+  url: string;
+  category: string;
+};
+
+async function get(): Promise<LinkStorage[]> {
+  const storage = await AsyncStorage.getItem(LINKS_STORAGE_KEY);
+  const reponse = storage ? JSON.parse(storage) : [];
+  return reponse;
+}
+
+async function save(newLink: LinkStorage) {
+  try {
+    const storage = await get();
+    const updated = JSON.stringify([...storage, newLink]);
+    await AsyncStorage.setItem(LINKS_STORAGE_KEY, updated);
+  } catch (error) {
+    throw error;
+  }
+}
