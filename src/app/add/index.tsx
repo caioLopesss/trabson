3c908c4049dd048f32c1aaa14852f009aa3d36be
Caio Lopes <caiolopeis95@gmail.com>
2025-02-11 16:02:50 -0300

    chore: botao de confirmaçao e volta pra pagina inicial

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -31,6 +31,9 @@ export default function Add() {
         url,
         category,
       });
+      Alert.alert("Sucesso", "Novo link adicionado", [
+        { text: "Ok", onPress: () => router.back() },
+      ]);
     } catch (error) {
       Alert.alert("erro", "Nao foi possivel salvar o link");
       console.log("error");
